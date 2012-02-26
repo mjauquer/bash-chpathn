d62ee91725f088354ae60cbc45ba1211338e3036
Marcelo Auquer <marce@cym.localdomain>
2012-02-25 21:53:31 -0300

    Add usage function and comments

--- chpathn.sh
+++ chpathn.sh
@@ -7,21 +7,64 @@
 #
 #  DESCRIPTION: Change filenames in the operand directory.
 #
-#      OPTIONS:
-# REQUIREMENTS:
-#         BUGS:
-#        NOTES:
+#      OPTIONS: See function 'usage' below.
+# REQUIREMENTS: upvars.sh
+#         BUGS: --
+#        NOTES: --
 #       AUTHOR: Marcelo Auquer, auquer@gmail.com
-#      CREATED:
-#     REVISION:
+#      CREATED: 02/19/2012
+#     REVISION: 02/25/2012
 #
 #======================================================================= 
 
 #===  FUNCTION =========================================================
 #
-#        NAME: portable
+#        NAME: asciivowels
 #
-#       USAGE: portable PATHNAME PATTERN VARNAME
+#       USAGE: asciivowels PATHNAME PATTERN VARNAME
+#
+# DESCRIPTION: Use the expanded value of PATTERN (the parent_matcher) to
+#              match a beggining substring (a prefix) of the expanded
+#              value of PATHNAME (pathname) to be preserved of further
+#              editing of this function. Replace in the trailing
+#              substring not matched by the parent_matcher (the suffix)
+#              every character of a non-ascii vowel with his matching
+#              ascii vowel character. Store the resulting string in the
+#              caller's variable VARNAME.
+#
+#  PARAMETERS: PATHNAME (A string).
+#              PATTERN  (A word subject of tilde expansion, parameter
+#                        expansion, command substitution and arithmetic
+#                        substitution). 
+#              VARNAME  (A variable name).
+#=======================================================================
+usage () {
+	cat <<- EOF
+	Usage: chpathn.sh [-b] [-c] [-h] [-p] [-r] [-R] [-s] [-t] [-v]\
+	DIR
+	
+	Change the name of files and subdirectories in DIR.
+
+	-b replace blank characters with underscores.
+	-c remove control characters.
+	-h display this help.
+	-p equivalent to -ctbvsrt
+	-r replace every sequence of more than one dash (or underscore)
+	   with only one character.
+	-R do all actions recursively.
+	-s replace every character with a dash, unless it is an
+	   alphanumeric character, a dot or an underscore.
+	-t remove leading dashes or blank characters and trailing blank
+	   characters.
+	-v replace non-ascii vowels with ascii ones.
+	EOF
+}
+
+#===  FUNCTION =========================================================
+#
+#        NAME: asciivowels
+#
+#       USAGE: asciivowels PATHNAME PATTERN VARNAME
 #
 # DESCRIPTION: Use the expanded value of PATTERN (the parent_matcher) to
 #              match a beggining substring (a prefix) of the expanded
@@ -66,35 +109,6 @@ asciivowels () {
 
 #===  FUNCTION =========================================================
 #
-#        NAME: portable
-#
-#       USAGE: portable PATHNAME PATTERN VARNAME
-#
-# DESCRIPTION: Use the expanded value of PATTERN (the parent_matcher) to
-#              match a beggining substring (a prefix) of the expanded
-#              value of PATHNAME (pathname) to be preserved of further
-#              editing of this function. Replace in the trailing
-#              substring not matched by the parent_matcher (the suffix)
-#              every character with a dash, unless it is a dot, an
-#              underscore or an alphanumeric character. Store the
-#              resulting string in the caller's variable VARNAME.
-#
-#  PARAMETERS: PATHNAME (A string).
-#              PATTERN  (A word subject of tilde expansion, parameter
-#                        expansion, command substitution and arithmetic
-#                        substitution). 
-#              VARNAME  (A variable name).
-#=======================================================================
-portable () {
-	local suffix="${1#$2}"
-	local prefix="${1%$suffix}"
-	suffix="${suffix//[^[:word:].]/-}"
-	local $3 && upvar $3 "$prefix$suffix"
-	trim "$new_name" "$2" "new_name"
-}
-
-#===  FUNCTION =========================================================
-#
 #        NAME: noblanks
 #
 #       USAGE: noblanks PATHNAME PATTERN VARNAME
@@ -118,6 +132,35 @@ noblanks () {
 	local prefix="${1%$suffix}"
 	suffix="${suffix//[[:blank:]]/_}"
 	local $3 && upvar $3 "$prefix$suffix"
+}
+
+#===  FUNCTION =========================================================
+#
+#        NAME: nospecial
+#
+#       USAGE: nospecial PATHNAME PATTERN VARNAME
+#
+# DESCRIPTION: Use the expanded value of PATTERN (the parent_matcher) to
+#              match a beggining substring (a prefix) of the expanded
+#              value of PATHNAME (pathname) to be preserved of further
+#              editing of this function. Replace in the trailing
+#              substring not matched by the parent_matcher (the suffix)
+#              every character with a dash, unless it is a dot, an
+#              underscore or an alphanumeric character. Store the
+#              resulting string in the caller's variable VARNAME.
+#
+#  PARAMETERS: PATHNAME (A string).
+#              PATTERN  (A word subject of tilde expansion, parameter
+#                        expansion, command substitution and arithmetic
+#                        substitution). 
+#              VARNAME  (A variable name).
+#=======================================================================
+nospecial () {
+	local suffix="${1#$2}"
+	local prefix="${1%$suffix}"
+	suffix="${suffix//[^[:word:].]/-}"
+	local $3 && upvar $3 "$prefix$suffix"
+	trim "$new_name" "$2" "new_name"
 }
 
 #===  FUNCTION =========================================================
@@ -171,11 +214,67 @@ nocntrl () {
 norep () {
 	local suffix="${1#$2}"
 	local prefix="${1%$suffix}"
-	suffix="${suffix//[[:cntrl:]]}"
 	suffix="$(echo $suffix | tr -s [-_])"
 	local $3 && upvar $3 "$prefix$suffix"
 	trim "$new_name" "$2" "new_name"
 
+}
+
+#===  FUNCTION =========================================================
+#
+#        NAME: portable
+#
+#       USAGE: portable PATHNAME PATTERN VARNAME
+#
+# DESCRIPTION: Equivalent to:
+#              {
+#              nocntrl PATHNAME PATTERN VARNAME
+#              trim PATHNAME PATTERN VARNAME
+#              noblanks PATHNAME PATTERN VARNAME
+#              asciivowels PATHNAME PATTERN VARNAME
+#              nospecial PATHNAME PATTERN VARNAME
+#              norep PATHNAME PATTERN VARNAME
+#              trim PATHNAME PATTERN VARNAME
+#              }
+#
+#  PARAMETERS: PATHNAME (A string).
+#              PATTERN  (A word subject of tilde expansion, parameter
+#                        expansion, command substitution and arithmetic
+#                        substitution). 
+#              VARNAME  (A variable name).
+#=======================================================================
+portable () {
+	local suffix="${1#$2}"
+	local prefix="${1%$suffix}"
+	suffix="${suffix//[[:cntrl:]]}"
+	suffix="${suffix##+([-[[:space:]])}"
+	suffix="${suffix%%+([[:space:]])}"
+	suffix="${suffix//[[:blank:]]/_}"
+	suffix=${suffix//â/a}
+	suffix=${suffix//à/a}
+	suffix=${suffix//á/a}
+	suffix=${suffix//ä/a}
+	suffix=${suffix//ê/e}
+	suffix=${suffix//è/e}
+	suffix=${suffix//é/e}
+	suffix=${suffix//ë/e}
+	suffix=${suffix//î/i}
+	suffix=${suffix//ì/i}
+	suffix=${suffix//í/i}
+	suffix=${suffix//ï/i}
+	suffix=${suffix//ô/o}
+	suffix=${suffix//ò/o}
+	suffix=${suffix//ó/o}
+	suffix=${suffix//ö/o}
+	suffix=${suffix//û/u}
+	suffix=${suffix//ù/u}
+	suffix=${suffix//ú/u}
+	suffix=${suffix//ü/u}
+	suffix="${suffix//[^[:word:].]/-}"
+	suffix="$(echo $suffix | tr -s [-_])"
+	suffix="${suffix##+([-[[:space:]])}"
+	suffix="${suffix%%+([[:space:]])}"
+	local $3 && upvar $3 "$prefix$suffix"
 }
 
 #===  FUNCTION =========================================================
@@ -220,15 +319,17 @@ recursive=false
 # Parse command line options.
 #-----------------------------------------------------------------------
 optindex=0
-while getopts 'bcprstv' option; do
+while getopts 'bchprRstv' option; do
 	case "$option" in
 		b) editopts[$optindex]=b
 		   ;;
 		c) editopts[$optindex]=c
 		   ;;
+		h) editopts[$optindex]=h
+		   ;;
 		p) editopts[$optindex]=p
 		   ;;
-		r) recursive=true
+		R) recursive=true
 		   ;;
 		s) editopts[$optindex]=s
 		   ;;
@@ -243,6 +344,10 @@ done
 # Build the find command.
 #-----------------------------------------------------------------------
 shift $(($OPTIND-1))
+if [ ! -d "$1" ]; then
+	usage
+	exit
+fi
 if [ $recursive == "true" ]; then
 	find_opts="-depth"
 else
@@ -283,12 +388,19 @@ while IFS="" read -r -d "" file; do
 			nocntrl "$new_name" "$parent_matcher" \
 				"new_name"
 		fi
+		if [ $editopt == h ]; then
+			usage
+		fi
 		if [ $editopt == p ]; then
 			portable "$new_name" "$parent_matcher" \
 				"new_name"
 		fi
-		if [ $editopt == s ]; then
+		if [ $editopt == r ]; then
 			norep "$new_name" "$parent_matcher" \
+				"new_name"
+		fi
+		if [ $editopt == s ]; then
+			nospecial "$new_name" "$parent_matcher" \
 				"new_name"
 		fi
 		if [ $editopt == t ]; then
