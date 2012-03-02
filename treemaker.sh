5a859296549667b344b86efc20bf87618ec3c33b
Marcelo Auquer <marce@cym.localdomain>
2012-03-02 13:02:40 -0300

    Added treemaker.sh

---
+++ treemaker.sh
@@ -0,0 +1,20 @@
+#/bin/bash
+
+cd ~/testing/chpathn
+if [ $? -eq 0 ]; then
+	mkdir "./ leadspace"
+	touch "./ leadspace/ leadspaceFil"
+	mkdir "./	leadtab"
+	touch "./	leadtab/	leadtabFil"
+	mkdir "./-leaddash"
+	touch "./-leaddash/-leaddashFil"
+	mkdir "./|#leadsymbs"
+	touch "./|#leadsymbs/¬½leadsymbs"
+	mkdir "./trailspace "
+	touch "./trailspace /trailspaceFil "
+	mkdir "./trailtab	"
+	touch "./trailtab	/trailtabFil	"
+	mkdir "./middle space"
+	touch "./middle space/middle space Fil"
+	touch "âàáäêèéëîìíïôòóöûùúü"
+fi
